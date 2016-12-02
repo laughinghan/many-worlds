611854f9e5c21eac9dab36a2ab6da8a0e231c3b4
Han Seoul-Oh <laughinghan@gmail.com>
2016-12-02 00:15:02 -0800

    🤗🐑 Better error message for nonexistent branch

--- clean-worktree-add.sh
+++ clean-worktree-add.sh
@@ -1,7 +1,14 @@
+set -e # exit if anything fails
+cd /tmp/mathquill.git
+
+git rev-parse --verify "$branchname" >/dev/null 2>&1 || {
+  echo "No such branch: $branchname"
+  exit 1
+}
+
 # gotta clear out any pre-existing worktree because
 # who knows if it succeeded
-rm -rf $worktree_path
-cd /tmp/mathquill.git
+rm -rf "$worktree_path"
 git worktree prune
 
-git worktree add $worktree_path $branchname
+git worktree add "$worktree_path" "$branchname"
